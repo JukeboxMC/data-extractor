ca7a6c8a305811b3f6c891545d88514d48e8a160
Kaooot <37877491+Kaooot@users.noreply.github.com>
2024-07-24 16:25:12 +0200

    Send original packets

--- src/main/kotlin/org/jukeboxmc/extractor/session/ProxiedBedrockClientSession.kt
+++ src/main/kotlin/org/jukeboxmc/extractor/session/ProxiedBedrockClientSession.kt
@@ -3,10 +3,10 @@
  */
 package org.jukeboxmc.extractor.session
 
-import io.netty.util.ReferenceCountUtil
 import org.cloudburstmc.protocol.bedrock.BedrockClientSession
 import org.cloudburstmc.protocol.bedrock.BedrockPeer
 import org.cloudburstmc.protocol.bedrock.netty.BedrockPacketWrapper
+import org.cloudburstmc.protocol.bedrock.packet.UnknownPacket
 import org.cloudburstmc.protocol.common.PacketSignal
 import org.jukeboxmc.extractor.DataExtractor
 
@@ -22,7 +22,11 @@ class ProxiedBedrockClientSession(private val dataExtractor: DataExtractor, peer
         if (this.packetHandler == null) {
             println("Received packet without a packet handler for ${this.socketAddress}:${this.subClientId}: " + packet)
         } else if (this.packetHandler.handlePacket(packet).equals(PacketSignal.UNHANDLED)) {
-            this.dataExtractor.serverSession().sendPacket(ReferenceCountUtil.retain(packet)!!)
+            val buffer = wrapper!!.packetBuffer.retainedSlice().skipBytes(wrapper.headerLength)
+            val pk = UnknownPacket()
+            pk.payload = buffer
+            pk.packetId = wrapper.packetId
+            this.dataExtractor.serverSession().sendPacket(pk)
         }
     }
 }
